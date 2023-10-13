5a749d661cd67dbebc72f1906ee1af5cd86222db
Gagan IK <ik.gagan@gmail.com>
2023-10-12 21:06:24 -0400

    outline for the calculator

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({value}) => {
+  return (
+    <button className='button'>
+        {value}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Screen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Screen = () => {
+  return (
+    <div className='screen'>0561654</div>
+  )
+}
+
+export default Screen
